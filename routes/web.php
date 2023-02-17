84f50b076f03adc5e9172f40e684207aedbb93b5
Farkhat <epalzyorinchos@mail.ru>
2023-02-17 20:23:21 +0600

    Single method controllers

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyPlaceController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,18 +14,20 @@ use App\Http\Controllers\ProductController;
 |
 */
 
+Route::group(['namespace' => 'App\Http\Controllers\Post'], function () {
+    Route::get('/posts', 'IndexController')->name('posts.index');
+
+    Route::get('/posts/create', 'CreateController')->name('posts.create');
+
+    Route::post('/posts', 'StoreController')->name('posts.store');
+
+    Route::get('/posts/{post}', 'ShowController')->name('posts.show');
+
+    Route::get('/posts/{post}/edit', 'EditController')->name('posts.edit');
+
+    Route::patch('/posts/{post}', 'UpdateController')->name('posts.update');
+
+    Route::delete('/posts/{post}', 'DestroyController')->name('posts.destroy');
+});
+
 Route::get('/', [MyPlaceController::class, 'index'])->name('index');
-
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-
-Route::patch('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
