0ebff639a1c35473b5fe45f37f89d3d640596181
Farkhat <epalzyorinchos@mail.ru>
2023-02-21 16:12:04 +0600

    Seeder and Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +18,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Category::factory(20)->create();
+        $posts = Post::factory(100)->create();
+        $tags = Tag::factory(50)->create();
+
+        foreach ($posts as $post) {
+            $tagIds = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagIds);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
